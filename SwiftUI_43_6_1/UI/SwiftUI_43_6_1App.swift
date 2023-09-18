ce9efb6627b6ef7266aabfaf97342165379097b5
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-09-18 03:18:32 +0200

    Changes at struct SwiftUI_43_6_1App

--- SwiftUI_43_6_1/UI/SwiftUI_43_6_1App.swift
+++ SwiftUI_43_6_1/UI/SwiftUI_43_6_1App.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct SwiftUI_43_6_1App: App {
+    
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
+        
     }
+    
 }
